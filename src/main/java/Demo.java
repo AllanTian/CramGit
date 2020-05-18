403ad7bc0dae0818fdd31c8fc1828ebab2b217a5
wht <1067829356@qq.com>
2020-05-18 15:44:36 +0800

    First Comiit

---
+++ src/main/java/Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: 马玉莲
+ * @Description:
+ * @Date: 15:43 2020/5/18
+ */
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Cram Git");
+    }
+}
